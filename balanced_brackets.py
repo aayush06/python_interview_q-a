40f303a4497c6b36d5e505907107fb9d9f5e3446
aayush <aayushbhatnagar06@gmail.com>
2018-07-26 14:50:14 +0530

    balanced bracket logic fixed

--- balanced_brackets.py
+++ balanced_brackets.py
@@ -3,13 +3,27 @@
     returns whether it is balanced. Every "opening" bracket must be followed by a closing bracket of the same type. 
     There can also be nested brackets, which adhere to the same rule.
 '''
-from collections import Counter
-
-a = "()[]{}(([])){[()][]}"
-
-x = Counter(a)
-
-if x['(']==x[')'] and x['{'] == x['}'] and x['['] == x[']']:
-    print True
-else:
-    print False
+import sys
+a = "[(])"
+a = list(a)
+x = []
+for i in a:
+    len_x = len(x)
+    if x == [] and i in ['(','{','[']:
+        x.append(i)                   
+    elif x != []:                     
+        if i in ['(', '{', '[']:
+            x.append(i)         
+        elif i == ')' and x[len_x-1] == '(':
+            x.pop()                         
+        elif i == '}' and x[len_x-1] == '{':
+            x.pop()                         
+        elif i== ']' and x[len_x-1] == '[': 
+            x.pop()                        
+        else:                              
+            print 'unbalanced'
+            sys.exit(0)       
+    else:                     
+        print 'unbalanced'
+        sys.exit(0)       
+print 'balanced'
